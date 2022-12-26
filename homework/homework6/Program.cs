856f86effe6102cf707386a60b7b84ed9ea4f1a4
vadim snigirev <snvad@mail.ru>
2022-12-26 20:21:23 +0300

    Дозагружены файлы program к заданиям

---
+++ homework/homework6/Program.cs
@@ -0,0 +1,150 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// int[] createArray(string inputArray) //функция из прошлых дз, переводит строку в массив чисел
+// {
+//     int i = 0;
+//     int size = 0;
+//     inputArray = inputArray + " ";
+//     while (i < inputArray.Length - 1) //узнаю длину нового массива чисел, так как не знаю можно ли задать динамический размер массиву
+//     {
+//         if (Char.IsDigit(inputArray[i]) == true)
+//         {
+//             int j = 1;
+//             while (Char.IsDigit(inputArray[i + j]) == true)
+//             {
+//                 j++;
+
+//             }
+//             i = i + j + 1;
+//             size = size + 1;
+//         }
+//         else
+//         {
+//             i++;
+//         }
+
+//     }
+//     int[] numArray = new int[size]; // объявляю массив нужного размера и заполняю его
+//     size = 0;
+//     i = 0;
+//     while (i < inputArray.Length)
+//     {
+//         if (Char.IsDigit(inputArray[i]) == true || inputArray[i] == '-')
+//         {
+//             int j = 1;
+//             string temp = $"{inputArray[i]}";
+//             while (Char.IsDigit(inputArray[i + j]) == true) 
+//             {
+//                 temp = temp + inputArray[i + j];
+//                 j++;
+//             }
+//             numArray[size] = Convert.ToInt32(temp);
+//             i = i + j;
+//             size = size + 1;
+//         }
+//         else
+//         {
+//             i++;
+//         }
+
+//     }
+//     return numArray;
+// }
+
+
+// void ShowArray(int[] array)
+// {
+//     Console.WriteLine("Полученный массив: ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int checkPositiveNum(int[] array)
+// {
+//     int rezult = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             rezult++;
+//         }
+//     }
+//     return rezult;
+// }
+
+// Console.WriteLine("Введите элементы массива (например 1,5,6,11):");
+// int[] inputArray = createArray(Console.ReadLine());
+// ShowArray(inputArray);
+// Console.WriteLine($"{checkPositiveNum(inputArray)} - положительных чисел ввел пользователь");
+
+// __________________________________________________________________________________________________________________
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] pointOfIntersection(double b1, double k1, double b2, double k2)
+{
+    double[] result = new double[3];
+    if (k1 != k2)
+    {
+        result[0] = Math.Round((-b1 + b2) / (k1 - k2), 2);
+        result[1] = Math.Round((k1 * result[0] + b1), 2);
+        result[2] = 1;
+        return result;
+    }
+    else
+    {
+        if (b1 != b2)
+        {
+            result[2] = 2;
+            return result;
+        }
+        else
+        {
+            result[2] = 0;
+            return result;
+        }
+    }
+}
+Console.WriteLine("Введите b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+double[] IntPoint = pointOfIntersection(b1, k1, b2, k2);
+
+if (IntPoint[2] == 1)
+{
+    Console.WriteLine($"({IntPoint[0]}, {IntPoint[1]}) - точка переcечения двух прямых заданных уравнениями:");
+    Console.WriteLine($"y = {k1}x + {b1}");
+    Console.WriteLine($"y = {k2}x + {b2}");
+}
+else
+{
+    if (IntPoint[2] == 0)
+    {
+        Console.WriteLine("Прямые заданные уравнениями ниже - совпадают");
+        Console.WriteLine($"y = {k1}x + {b1}");
+        Console.WriteLine($"y = {k2}x + {b2}");
+    }
+    else
+    {
+        Console.WriteLine("Прямые заданные уравнениями ниже - не пересекаются");
+        Console.WriteLine($"y = {k1}x + {b1}");
+        Console.WriteLine($"y = {k2}x + {b2}");
+    }
+}
+
+
---
+++ seminars/seminar6/Program.cs
@@ -0,0 +1,136 @@
+﻿// //Задача 1. Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте,
+// //а первый - на последнем и т.д.)
+
+// //[1 2 3 4 5] -> [5 4 3 2 1]
+// //[6 7 3 6] -> [6 3 7 6]
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] newArray = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         newArray[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return newArray;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     Console.WriteLine("Полученный массив: ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int[] sortingArray(int[] array)
+// {
+//     for (int i=0;i<array.Length/2; i++)
+//     {
+//         int temp = array[i];
+//            array[i] = array[array.Length-1-i];
+//            array[array.Length-1-i] = temp;
+//     }
+//     return array;
+// }
+// Console.WriteLine("Введите размер массива");
+// int length = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимально возможное значение элемента массива");
+// int minNum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение элемента массива");
+// int maxNum = Convert.ToInt32(Console.ReadLine());
+// int[] newAr = CreateRandomArray(length, minNum,maxNum);
+// ShowArray(newAr);
+// sortingArray(newAr);
+// ShowArray(newAr);
+
+//Задача 2.Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+//самое важное знать правило, какждая сторона должна быть меньше(строго) суммы двух других сторон
+
+// Boolean checkTriangle (int x, int y, int z)
+// {
+//     if (x+y>z && x+z>y && z+y>x)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+    
+
+// }
+// Console.WriteLine("Введите длину первой стороны:");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите длину второйстороны:");
+// int y = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите длину третьей стороны:");
+// int z = Convert.ToInt32(Console.ReadLine());
+// if (checkTriangle(x,y,z) == true)
+// {
+//     Console.WriteLine($"Треугольник со сторонами длиной {x},{y},{z} существует");
+// }
+// else
+// {
+//     Console.WriteLine($"Треугольник со сторонами длиной {x},{y},{z} не существует");
+// }
+
+
+// Задача 3. Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: a и b.
+
+// числа фибоначчи последовательность чисел в которой первые 2 числа это 0 и 1, а последующие это сумма двух передыдущих
+// [0, 1, 1, 2, 3, 5, 8, 13, ... ]
+// f(n) = f(n-1) + f(n-2)
+
+// int[] fibonacciArray(int size, int a, int b)
+// {
+//     int[] array = new int[size+1];
+//     array[0] = a;
+//     array[1] = b;
+//     for (int i=2; i<array.Length;i++)
+//     {
+//         array[i] = array[i-1] + array[i-2];
+//     }
+//     return array;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     Console.WriteLine("Полученный массив: ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Какое количество чисел последовательности Фибоначчи, после введенных 2 чисел, необходимо вывести на экран?:");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число последовательности:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число последовательности:");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// ShowArray(fibonacciArray(size,a,b));
+
+
+// Задача 4. ****Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+
+string TenToTwo(int n)
+{
+    string rezult = string.Empty;
+    while (n != 0)
+    {
+        rezult = n % 2 + rezult;
+        n = n / 2;
+    }
+    return rezult;
+}
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{n} - это {TenToTwo(n)} в двоичной системе");

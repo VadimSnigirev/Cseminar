1a76c7f4099ea2497a5147ec12c9bf2fd010f213
vadim snigirev <snvad@mail.ru>
2023-01-04 08:40:15 +0300

    Сделано дз 9 семинара

--- homework/homework9/Program.cs
+++ homework/homework9/Program.cs
@@ -43,16 +43,6 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-// int Akkerman(int n, int m)
-// {
-//     if (n == 0)
-//         return m + 1;
-//     else
-//       if ((n != 0) && (m == 0))
-//         return Akkerman(n - 1, 1);
-//     else
-//         return Akkerman(n - 1, Akkerman(n, m - 1));
-// }
 int Akkerman(int m, int n)
 {
     if (m == 0)

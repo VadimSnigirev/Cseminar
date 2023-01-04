25ebfcf74cd2967a53ddc4dc33cf1b4a758cbf01
vadim snigirev <snvad@mail.ru>
2023-01-04 08:37:51 +0300

    Сделано дз 9 семинара

---
+++ homework/homework9/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// void PrintAllNaturalNumbers (int num)
+// {
+//     if (num > 1) 
+//     {
+//         Console.Write(num+", "); 
+//         PrintAllNaturalNumbers(num-1);
+//     }
+//     else { Console.Write(num);}
+// }
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// PrintAllNaturalNumbers(num);
+
+// _______________________________________________________________________________________________________________________________________________
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// int SumBetweenNumbers (int lowerValue, int greaterValue)
+// {
+//     if (lowerValue < greaterValue) 
+//     return SumBetweenNumbers(lowerValue+1,greaterValue)+lowerValue; 
+//     else
+//     return greaterValue;  
+// }
+// Console.WriteLine("Введите первое число:");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (M < N) Console.WriteLine(SumBetweenNumbers(M,N));
+// else  Console.WriteLine(SumBetweenNumbers(N,M));
+
+// ___________________________________________________________________________________________________________________
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// int Akkerman(int n, int m)
+// {
+//     if (n == 0)
+//         return m + 1;
+//     else
+//       if ((n != 0) && (m == 0))
+//         return Akkerman(n - 1, 1);
+//     else
+//         return Akkerman(n - 1, Akkerman(n, m - 1));
+// }
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+      if ((m != 0) && (n == 0))
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+ 
+Console.WriteLine(Akkerman(2, 3));
+Console.WriteLine(Akkerman(3, 2));
---
+++ seminars/seminar9/Program.cs
@@ -0,0 +1,83 @@
+﻿//Задача 1. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+//N = 5 -> "1, 2, 3, 4, 5"
+//N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// void PrintAllNaturalNumbers (int num)
+// {
+//     if (num > 1) 
+//     {
+//         PrintAllNaturalNumbers(num-1);
+//         Console.Write($", {num}"); 
+//     }
+//     else
+//     { 
+//         Console.Write($"{num}");  
+//     }
+// }
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+// PrintAllNaturalNumbers(num);
+
+//Задание 2. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+//453 -> 12
+//45 -> 9
+
+// int SumOfDigits(int num)
+// {
+//     if (num > 9)
+//     {
+//        return SumOfDigits(num/10) + num%10;
+//     }
+//     else { return num;}
+// }
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+// Console.WriteLine($"{SumOfDigits(num)} - сумма цифр числа {num}");
+
+//Задача 3. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+//M = 1; N = 5. -> "1, 2, 3, 4, 5"
+//M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+// void PrintNumberFromMintoMax (int minValue, int maxValue)
+// {
+//     if (minValue < maxValue) 
+//     {
+//         Console.Write($"{minValue}, ");
+//         PrintNumberFromMintoMax(minValue+1,maxValue); 
+//     }
+//     else
+//     { 
+//         Console.Write($"{maxValue}");  
+//     }
+// }
+// Console.WriteLine("Введите первое число:");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (M < N) { PrintNumberFromMintoMax(M,N); }
+// else { PrintNumberFromMintoMax(N,M); }
+
+//Задача 4.Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+int Exponentiate(int numbers, int degreeOf)
+{
+    if (degreeOf != 0)
+    {
+        if (degreeOf > 1)
+        return Exponentiate(numbers, degreeOf-1) * numbers;
+        else  return numbers; 
+    }
+    else return 1;
+}
+Console.WriteLine("Введите число, которое нужно возвести в степень:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень в которую нужно возвести число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Exponentiate(a,b));
+
+
